6d92591dca01ed17df27f932a8e380450cf6eac2
Qwertu-Hacker <109757033+Qwertu-Hacker@users.noreply.github.com>
2023-09-11 19:45:29 +0300

    created empty project

---
+++ RiderPDF/RiderPDF/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RiderPDF
+//
+//  Created by Igor Kononov on 11.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .cyan
+    }
+
+
+}
+
